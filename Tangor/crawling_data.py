225e1bd182b4343e249581bdf3229725310ba890
vanilla028 <145339245+vanilla028@users.noreply.github.com>
2023-10-10 01:46:43 +0900

    Update crawling_data.py

--- Tangor/crawling_data.py
+++ Tangor/crawling_data.py
@@ -1,19 +1,14 @@
+# chromedriver.exe 설치 필요
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys # 키보드 역할
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from multiprocessing import Pool
 import pandas as pd
-import os            # 이미지 저장을 위한 폴더 생성 관리
+import os
 import time
-import urllib.request # 이미지 다운로드를 위한 라이브러리
+import urllib.request
 
 # 키워드 가져오기
-# keys = pd.read_csv("./keyword.txt", encoding="utf-8", names=['keyword'])
+keys = pd.read_csv("./keyword.txt", encoding="utf-8", names=['keyword'])
 """
       keyword
 0     dekopon
@@ -22,10 +17,8 @@ import urllib.request # 이미지 다운로드를 위한 라이브러리
 3     orange
 4     cheonhyehyang
 """
-keys = {0: "dekopon", 1: "grapefruit", 2: "grapefruit", 3: "kanpei", 4: "orange", 5: "cheonhyehyang"}
 
-# keyword = [keyword.append(keys['keyword'][x]) for x in range(len(keys))]
-keyword = [keys[x] for x in range(len(keys))]
+keyword = keys['keyword'].tolist()
 
 def create_folder(dir):
     # 이미지 저장할 폴더 구성
@@ -41,17 +34,14 @@ def image_download(keyword):
     create_folder("./" + keyword + "/")
 
     # chromdriver 가져오기
-    options = Options()
-    # options.add_argument('--headless') # 백그라운드 실행
-    options.add_experimental_option("detach", True) # 세션 종류 후에도 브라우저 창 유지하기
-      
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.implicitly_wait(3) # 페이지 로딩이 완료될 때까지 기다리는 코드
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome("./chromedriver.exe", options=options)
+    # windows
+    # chromdriver = "./chromedriver.exe"
+    driver.implicitly_wait(3)
 
     print("keyword: " + keyword)
-      
-    # 사이트 접속하기  
     driver.get('https://www.google.co.kr/imghp?hl=ko')
     keywords = driver.find_element_by_xpath(
         '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
@@ -77,7 +67,7 @@ def image_download(keyword):
     images = driver.find_elements_by_css_selector("img.rg_i.Q4LuWd")
     print(keyword+' 찾은 이미지 개수:', len(images))
 
-    links = [] # 다운로드할 이미지 링크 수집
+    links = []
     for i in range(1, len(images)):
         try:
             print('//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img')
@@ -91,7 +81,7 @@ def image_download(keyword):
         except:
             continue
 
-    forbidden = 0 # 이미지 다운로드 시 발생하는 예외 계산
+    forbidden = 0
     for k, i in enumerate(links):
         try:
             url = i
